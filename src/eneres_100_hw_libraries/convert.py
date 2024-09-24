d969274aa9f43f3bfd81893efb976a99146244bf
Serrindipity <jon.quitoriano@gmail.com>
2024-09-24 15:55:38 -0700

    change import statement to allow running from venv interpreter

--- src/eneres_100_hw_libraries/convert.py
+++ src/eneres_100_hw_libraries/convert.py
@@ -1,4 +1,4 @@
-from UniversalNumbers import _parse_input_type
+from src.eneres_100_hw_libraries.UniversalNumbers import _parse_input_type
 from unum import Unum
 def convert(input) -> Unum:
     return _parse_input_type(input)
