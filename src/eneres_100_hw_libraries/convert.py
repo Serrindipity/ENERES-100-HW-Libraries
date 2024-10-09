c1d63a02cb7315e494f80ef483fe9a2950ac1a01
Serrindipity <jon.quitoriano@gmail.com>
2024-10-08 19:18:16 -0700

    update so it doesn't suck

--- src/eneres_100_hw_libraries/convert.py
+++ src/eneres_100_hw_libraries/convert.py
@@ -1,4 +1,6 @@
-from src.eneres_100_hw_libraries.UniversalNumbers import _parse_input_type
+from UniversalNumbers import _parse_input_type
 from unum import Unum
+
+
 def convert(input) -> Unum:
     return _parse_input_type(input)
