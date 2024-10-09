96f6c652862ed153dddff16b4211263fa20e1c1d
Serrindipity <jon.quitoriano@gmail.com>
2024-10-08 23:47:10 -0700

    update word representations

--- eneres_100_hw_libraries/UniversalNumbers.py
+++ eneres_100_hw_libraries/UniversalNumbers.py
@@ -12,96 +12,102 @@ verbose = False
 """
 Returns a Unum object from a variety of number representations. Parses data, then tokenizes, then runs eval() on tokens
 """
-def _parse_input_type(input: float|int|str|Decimal|Unum) -> Unum:
-        getcontext().prec=7
-        
-        if isinstance(input, (float, int)):
-            return Unum.coerceToUnum(Decimal(input))
-        
-        if isinstance(input, (Decimal, Unum)):
-            return Unum.coerceToUnum(input)
-
-        # Parse comma-separated stuff
-        if isinstance(input, str) and "," in input:
-            input = input.replace(',', '')
-
-        # Handle percents
-        if isinstance(input, str) and input.endswith("%"):
-            try:
-                dec = Decimal(input[:-1]) / 100
-            except InvalidOperation:
-                raise ValueError(f"Invalid percentage format: {input}")
-            return Unum.coerceToUnum(dec)
-        # Do conversions:
-        
-        conversions = {
-            "million" : Decimal(10**6),
-            "billion" : Decimal(10**9),
-            "trillion" : Decimal(10**12),
-            "quadrillion" : Decimal(10**15),
-            "quintillion" : Decimal(10**18),
-            "sextillion" : Decimal(10**21),
-            "per" : "/",
-            'x' : '*',
-            '^' : "**",
-        }
-        operators = ['+', '-', '*', "**", '/']
-
-        
-        in_str = input.strip()
-        tokens = tokenize(in_str, None)
-        if verbose:
-            print(tokens)
-        converted_tokens = []
-        for i in tokens:
-            if i in conversions.keys():
-                converted_tokens.append(conversions[i])
-            else:
-                try:
-                    if verbose:
-                        print(i)
-                    converted_tokens.append(Decimal(i))
-                except (InvalidOperation, TypeError):
-                    converted_tokens.append(i)
-                    # print("Decimal coercion failed")
-        if verbose:
-            print(f"converted tokens = {converted_tokens}")
-        final_tokens = []
-        # Add INSERT_OPERATOR between all tokens without operators between them
-        for prev, current in zip(converted_tokens, converted_tokens[1:]):
-                final_tokens.append(prev)
-                if prev not in operators and current not in operators:
-                    final_tokens.append(UniversalNumber.INSERT_OPERATOR)
-        final_tokens.append(converted_tokens[-1])
-
-        input = ''.join(repr(i) if not isinstance(i, str) else i for i in final_tokens)
-        if verbose:
-            print(f"Post conversion output is {input}")
 
 
-        # Allow only safe operations and scientific notation and Unum units
-        allowed_globals = {"__builtins__": None}
-        allowed_locals = {
-            "Decimal": Decimal,
-            "e": Decimal("2.718281828459045"),
-            "pi": Decimal("3.141592653589793"),
-            "math": __import__('math')  # Import math for math constants if necessary
-        }
-        # Add units as allowed_locals
-        allowed_locals.update({k: v for k, v in vars(u).items() if not k.startswith('_')})
+def _parse_input_type(input: float | int | str | Decimal | Unum) -> Unum:
+    getcontext().prec = 7
+
+    if isinstance(input, (float, int)):
+        return Unum.coerceToUnum(Decimal(input))
+
+    if isinstance(input, (Decimal, Unum)):
+        return Unum.coerceToUnum(input)
+
+    # Parse comma-separated stuff
+    if isinstance(input, str) and "," in input:
+        input = input.replace(",", "")
+
+    # Handle percents
+    if isinstance(input, str) and input.endswith("%"):
         try:
-            
-            # Evaluate the input safely
-            evaluated_input = eval(input, allowed_globals, allowed_locals)
-            
-            # Convert the result to Decimal
-            return evaluated_input if isinstance(evaluated_input, Unum) else Unum.coerceToUnum(Decimal(evaluated_input))
-        except (TypeError, InvalidOperation):
-            raise ValueError(f"Invalid number format: {input}")
+            dec = Decimal(input[:-1]) / 100
+        except InvalidOperation:
+            raise ValueError(f"Invalid percentage format: {input}")
+        return Unum.coerceToUnum(dec)
+    # Do conversions:
+
+    conversions = {
+        "hundred": Decimal(10**2),
+        "thousand": Decimal(10**3),
+        "million": Decimal(10**6),
+        "billion": Decimal(10**9),
+        "trillion": Decimal(10**12),
+        "quadrillion": Decimal(10**15),
+        "quintillion": Decimal(10**18),
+        "sextillion": Decimal(10**21),
+        "per": "/",
+        "x": "*",
+        "^": "**",
+    }
+    operators = ["+", "-", "*", "**", "/"]
+
+    in_str = input.strip()
+    tokens = tokenize(in_str, None)
+    if verbose:
+        print(tokens)
+    converted_tokens = []
+    for i in tokens:
+        if i in conversions.keys():
+            converted_tokens.append(conversions[i])
+        else:
+            try:
+                if verbose:
+                    print(i)
+                converted_tokens.append(Decimal(i))
+            except (InvalidOperation, TypeError):
+                converted_tokens.append(i)
+                # print("Decimal coercion failed")
+    if verbose:
+        print(f"converted tokens = {converted_tokens}")
+    final_tokens = []
+    # Add INSERT_OPERATOR between all tokens without operators between them
+    for prev, current in zip(converted_tokens, converted_tokens[1:]):
+        final_tokens.append(prev)
+        if prev not in operators and current not in operators:
+            final_tokens.append(UniversalNumber.INSERT_OPERATOR)
+    final_tokens.append(converted_tokens[-1])
+
+    input = "".join(repr(i) if not isinstance(i, str) else i for i in final_tokens)
+    if verbose:
+        print(f"Post conversion output is {input}")
+
+    # Allow only safe operations and scientific notation and Unum units
+    allowed_globals = {"__builtins__": None}
+    allowed_locals = {
+        "Decimal": Decimal,
+        "e": Decimal("2.718281828459045"),
+        "pi": Decimal("3.141592653589793"),
+        "math": __import__("math"),  # Import math for math constants if necessary
+    }
+    # Add units as allowed_locals
+    allowed_locals.update({k: v for k, v in vars(u).items() if not k.startswith("_")})
+    try:
+
+        # Evaluate the input safely
+        evaluated_input = eval(input, allowed_globals, allowed_locals)
+
+        # Convert the result to Decimal
+        return (
+            evaluated_input
+            if isinstance(evaluated_input, Unum)
+            else Unum.coerceToUnum(Decimal(evaluated_input))
+        )
+    except (TypeError, InvalidOperation):
+        raise ValueError(f"Invalid number format: {input}")
 
 
 class UniversalNumber:
-    INSERT_OPERATOR = '*'
+    INSERT_OPERATOR = "*"
 
     def __init__(self, input) -> None:
         self.dec = convert(input)
@@ -140,17 +146,14 @@ class UniversalNumber:
     def generate_reprs(self) -> dict:
         comma_repr = f"{self.dec:,}"
         sci_repr = "%e" % self.dec
-        return {
-            "dec": self.dec,
-            "comma": comma_repr,
-            "sci" : sci_repr
-        }
+        return {"dec": self.dec, "comma": comma_repr, "sci": sci_repr}
 
 
+def tokenize(expr: str, pattern: re.Pattern | None) -> list:
 
-def tokenize(expr: str, pattern: re.Pattern|None) -> list:
-
-    token_pattern = r"(\d+\.\d+|\d+|[a-zA-Z]+|\*\*|[+\-*/()^])" if not pattern else pattern
+    token_pattern = (
+        r"(\d+\.\d+|\d+|[a-zA-Z]+|\*\*|[+\-*/()^])" if not pattern else pattern
+    )
 
     tokens = re.findall(token_pattern, expr, re.VERBOSE)
     return tokens
